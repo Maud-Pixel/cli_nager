d4f42ca1749bf3fd59a6cf1dddc18bcc926c96e0
Maud-Pixel <leleuxmaud@gmail.com>
2021-01-25 16:39:19 +0100

    commit the end

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cli_nager
+
+### Description
+Application with a get query to retrieve holidays dates from all over the world. 
+Made in January 2021
+ 
+***
+### Author
+
+Maud Leleux, student developer Web for an exercise
+
+***
+
+### Use
+- node version 12.19.0
+- to use it put in your console the word "nager" a year (ex: 2020) and a country (ex: Belgium)
+
+$  nager *year* *country*
+
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+#!/usr/bin/env node
+
 console.log("Hello, Node.JS!");
+
+
+const axios = require('axios').default;
+const countryList = require('country-list');
+let argmt = process.argv.slice(2);
+let year = argmt[0];
+let name = argmt [1];
+
+async function getUser(year, name) {
+    try {
+        if(countryList.getCode(name)!= undefined)
+        {
+            let code = countryList.getCode(name);
+            const response = await axios.get('https://date.nager.at/api/v2/publicholidays/'+year + "/"+code+'');
+            console.log(response);
+        }
+        else
+        {
+            console.log("flute, votre code pays est incorrect");
+        } 
+      
+    } catch (error) {
+      console.error(error);
+      console.log("aie aie aie");
+      
+    }
+  }
+getUser(year,name);
+ 
+  
+  
